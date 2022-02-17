8daa57347f74a83c249289ee12e58174805cf128
rt099 <rio.thomas@yurika.com.au>
2022-02-17 12:19:54 +1000

    Add ability to generate outputs for HOMER.

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import glob
 
+import numpy as np
 from nemreader import output_as_data_frames
+import pandas as pd
 
 dfs = {}
+pvs = {}
 
 # For each NEM12 File:
 nem12_file_list = glob.glob('examples/NEM12*')
@@ -12,19 +15,14 @@ for file in nem12_file_list:
     df.insert(2, "Time", df["t_start"].dt.time)
 
     if nmi in dfs.keys():
-        dfs[nmi] = dfs[nmi].merge(df)
+        dfs[nmi] = pd.concat([dfs[nmi], df], sort=True)
     else:
         dfs[nmi] = df
 
-    # pv = pandas.pivot_table(df, values='E1', index='Date', columns='Time', aggfunc=np.max)
-
-# dfs = output_as_data_frames('examples/examples.zip')
-# # Format data for HOMER: day x half-hour:
-# for nmi, df in dfs:
-#     # print(nmi)
-#     df.insert(1, "Date", df["t_start"].dt.date)
-#     df.insert(2, "Time", df["t_start"].dt.time)
-#     # columns = list(df["t_start"].dt.time.drop_duplicates())
-#     pv = pandas.pivot_table(df, values='E1', index='Date', columns='Time', aggfunc=np.max)
+for nmi, df in dfs.items():
+    print(f"{nmi} - {df.shape}")
+    pv = pd.pivot_table(df, values='E1', index='Date', columns='Time', aggfunc=np.max)
+    pv.to_csv(f"output/{nmi}_load_profile.csv")
+    pvs[nmi] = pv
 
 print('fin')
